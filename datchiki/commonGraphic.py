5ae3d8f20a02f7d2871efbd58bd4ea72d95ceed0
Тарачёв Артём <tartm2003@gmail.com>
2024-10-26 17:17:50 +0700

    Added new import modules

---
+++ datchiki/commonGraphic.py
@@ -0,0 +1,11 @@
+import mysql.connector
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import pandas as pd
+import numpy as np
+from PIL import Image, ImageTk
+from datetime import datetime, timedelta
+import tkinter as tk
+from tkinter import Toplevel
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
